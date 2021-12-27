feabf941aea838f4b639a6efbae0e8bcda5c1ff4
SeungHwan-Lee <sh981013s@gmail.com>
2021-12-27 16:03:18 +0900

    feat: moved fetching func to App comp

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,19 @@ import SearchForm from './components/SearchForm';
 import SearchList from './components/SearchList';
 
 function App() {
+  const getData = (keyword: string) => {
+    console.log('키워드:' + keyword);
+    fetch(`api/data?keyword=${keyword}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  };
   return (
     <div className="App">
-      <SearchForm />
+      <SearchForm getData={getData} />
       <SearchList />
     </div>
   );
--- client/src/components/SearchForm.tsx
+++ client/src/components/SearchForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const fetchApi = () => {
   fetch('api/data')
     .then((res) => {
@@ -8,11 +10,34 @@ const fetchApi = () => {
     });
 };
 
-const SearchForm = () => {
+const SearchForm = (props: { getData: any }) => {
+  const { getData } = props;
+  const [keyword, setKeyword] = useState('');
   return (
     <div className="form">
-      <input type="text" className="form-text" />
-      <button type="button" className="form-btn" onClick={fetchApi}>
+      <input
+        type="text"
+        className="form-text"
+        onChange={(e: any) => {
+          setKeyword(e.target.value);
+        }}
+        onKeyPress={(e: any) => {
+          if (e.charCode === 13) {
+            if (keyword) {
+              getData(keyword);
+            }
+          }
+        }}
+      />
+      <button
+        type="button"
+        className="form-btn"
+        onClick={() => {
+          if (keyword) {
+            getData(keyword);
+          }
+        }}
+      >
         search
       </button>
     </div>
